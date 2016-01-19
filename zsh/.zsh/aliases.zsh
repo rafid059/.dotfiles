39c516d9d4dda69b12b471ef31f5ad5735796585
Rafiduzzaman Sonnet <sonnet@localhost.localdomain>
2016-01-19 12:25:10 +0600

    aliases for matlab, sublime and intellij added

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -1,1 +1,4 @@
 alias android_studio='/mnt/work/Dev-env/android/android-studio/android-studio/bin/studio.sh'
+alias subl='/mnt/work/Dev-env/sublime_text_3/sublime_text'
+alias matlab='sudo /usr/local/MATLAB/R2015b/bin/matlab'
+alias intellij='/mnt/work/Dev-env/idea/bin/idea.sh'
