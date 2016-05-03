c08c313c716270b43d5598acb93dc022ea4ec25b
Rafiduzzaman Sonnet <sonnet@localhost.localdomain>
2016-05-04 01:36:27 +0600

    added shutdown script

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -1,4 +1,6 @@
 alias android_studio='/mnt/work/Dev-env/android/android-studio/android-studio/bin/studio.sh'
 alias subl='/mnt/work/Dev-env/sublime_text_3/sublime_text'
-alias matlab='sudo /usr/local/MATLAB/R2015b/bin/matlab'
 alias intellij='/mnt/work/Dev-env/idea/bin/idea.sh'
+alias webstorm='/mnt/work/Dev-env/WebStorm/bin/webstorm.sh'
+alias phpstorm='/mnt/work/Dev-env/PhpStorm/bin/phpstorm.sh'
+alias blender='~/blender-2.77-linux-glibc211-x86_64/blender'
