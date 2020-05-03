fcb8f468275dedc8c8d5dcb19c217563421a0b5e
sonnet <sonnet@localhost.localdomain>
2020-05-03 15:30:41 +0200

    Delete everything

--- zsh/.zsh/aliases.zsh
+++
@@ -1,8 +0,0 @@
-alias android_studio='/mnt/work/Dev-env/android/android-studio/android-studio/bin/studio.sh'
-alias subl='/mnt/work/Dev-env/sublime_text_3/sublime_text'
-alias intellij='/mnt/work/Dev-env/idea/bin/idea.sh'
-alias clion='/mnt/work/Dev-env/clion/bin/clion.sh'
-alias webstorm='/mnt/work/Dev-env/WebStorm/bin/webstorm.sh'
-alias phpstorm='/mnt/work/Dev-env/PhpStorm/bin/phpstorm.sh'
-alias blender='~/blender-2.77-linux-glibc211-x86_64/blender'
-alias python=python3
--- zsh/.zsh/completion.zsh
+++
@@ -1,5 +0,0 @@
-# match uppercase from lowercase
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
-
-
-# add completion for mid-completion
